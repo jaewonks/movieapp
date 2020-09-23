353bb21648c6b9295e5bdf6af2d23fb6606ac1ff
Jaewon Kim <jaewon@Jaewons-MBP.cust.communityfibre.co.uk>
2020-09-23 19:52:02 +0100

    Updates

---
+++ precache-manifest.6cf2c67115ff457225337f135fa3c1f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3be2264692f29701e18bf6547412de65",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "7bdcb3c4e6601d0abfc3",
+    "url": "/movieapp/static/css/main.43f38196.chunk.css"
+  },
+  {
+    "revision": "260ba03d70a20222093f",
+    "url": "/movieapp/static/js/2.6208fde9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movieapp/static/js/2.6208fde9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bdcb3c4e6601d0abfc3",
+    "url": "/movieapp/static/js/main.2303b473.chunk.js"
+  },
+  {
+    "revision": "0d516f28c241e3c70098",
+    "url": "/movieapp/static/js/runtime-main.04b21085.js"
+  }
+]);
